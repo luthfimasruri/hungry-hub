c863a9e8a4e7d78933303fae8ceb6a232ec4a2fa
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2022-07-22 10:36:14 +0700

    feat: :rocket: add 404 page

---
+++ src/views/404View.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="not-found min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8"
+  >
+    <div class="mx-auto max-w-max">
+      <main class="sm:flex">
+        <p class="text-4xl font-extrabold text-red sm:text-5xl">404</p>
+        <div class="sm:ml-6">
+          <div class="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1
+              class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl"
+            >
+              Page not found
+            </h1>
+            <p class="mt-1 text-base text-gray-500">
+              Please check the URL in the address bar and try again.
+            </p>
+          </div>
+          <div
+            class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6"
+          >
+            <a
+              href="/"
+              class="inline-flex items-center rounded-md border border-transparent bg-red px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+            >
+              Go back home
+            </a>
+            <a
+              href="#"
+              class="inline-flex items-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-600 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+            >
+              Contact support
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<style>
+.not-found {
+  min-height: 85vh;
+  display: flex;
+  align-items: center;
+}
+</style>
