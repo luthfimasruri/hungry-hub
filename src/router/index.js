096c30c063bd38d46a26fc2dd1fe6e3b048c00cd
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2022-07-22 10:50:56 +0700

    chore: :lipstick: update 404 view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +12,8 @@ const router = createRouter({
     },
     {
       path: '/:pathMatch(.*)*',
-      name: '404',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/404View.vue'),
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
 })
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="not-found min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8"
+    class="not-found bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8"
   >
     <div class="mx-auto max-w-max">
       <main class="sm:flex">
