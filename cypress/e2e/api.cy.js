fb7c7a10b001110321bf2b5cb355b9b83c2873c4
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2022-07-24 09:30:07 +0700

    refactor: :recycle: move api test to a single file

---
+++ cypress/e2e/api.cy.js
@@ -0,0 +1,54 @@
+const apiUrl = Cypress.env('apiUrl')
+
+describe('Cities API', () => {
+  context('GET /api/v5/cities.json', () => {
+    it('gets a list of cities', () => {
+      cy.request('GET', `${apiUrl}/api/v5/cities.json`).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.success).to.eq(true)
+        expect(response.body.data).length.to.be.greaterThan(0)
+      })
+    })
+  })
+})
+
+describe('Banners API', () => {
+  context('GET /api/v5/banners.json?city_id=1', () => {
+    it('gets a list of banners', () => {
+      cy.request('GET', `${apiUrl}/api/v5/banners.json?city_id=1`).then(
+        (response) => {
+          expect(response.status).to.eq(200)
+          // expect(response.body.success).to.eq(true)
+          expect(response.body).length.to.be.greaterThan(0)
+        }
+      )
+    })
+  })
+})
+
+describe('Config API', () => {
+  context('GET /api/v5/config.json', () => {
+    it('gets an object of config', () => {
+      cy.request('GET', `${apiUrl}/api/v5/config.json`).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.success).to.eq(true)
+        expect(response.body.data).to.be.not.null
+      })
+    })
+  })
+})
+
+describe('Section3 API', () => {
+  context('GET /api/v5/homes/section_3.json?city_id=1', () => {
+    it('gets a list of section3', () => {
+      cy.request('GET', `${apiUrl}/api/v5/homes/section_3.json?city_id=1`).then(
+        (response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body.success).to.eq(true)
+          expect(response.body.title).to.be.not.null
+          expect(response.body.data).length.to.be.greaterThan(0)
+        }
+      )
+    })
+  })
+})
