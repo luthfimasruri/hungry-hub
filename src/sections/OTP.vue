34b95e99e25e4b3701be78315a67f9d28322e859
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2022-08-25 17:45:51 +0700

    aldsfj

--- src/sections/OTP.vue
+++ src/sections/OTP.vue
@@ -124,30 +124,32 @@ export default {
       }
     },
     initOtp2() {
-      const input = document.querySelector(
-        'input[autocomplete="one-time-code"]'
-      )
-      if (!input) return
-      const ac = new AbortController()
-      const form = input.closest('form')
-      if (form) {
-        form.addEventListener('submit', (e) => {
-          ac.abort()
-        })
+      if ('OTPCredential' in window) {
+        const input = document.querySelector(
+          'input[autocomplete="one-time-code"]'
+        )
+        if (!input) return
+        const ac = new AbortController()
+        const form = input.closest('form')
+        if (form) {
+          form.addEventListener('submit', () => {
+            ac.abort()
+          })
+        }
+        navigator.credentials
+          .get({
+            otp: { transport: ['sms'] },
+            signal: ac.signal,
+          })
+          .then((otp) => {
+            alert('otp', otp.code)
+            input.value = otp.code
+            if (form) form.submit()
+          })
+          .catch((err) => {
+            console.log(err)
+          })
       }
-      navigator.credentials
-        .get({
-          otp: { transport: ['sms'] },
-          signal: ac.signal,
-        })
-        .then((otp) => {
-          alert('otp', otp.code)
-          input.value = otp.code
-          if (form) form.submit()
-        })
-        .catch((err) => {
-          console.log(err)
-        })
     },
   },
 }
